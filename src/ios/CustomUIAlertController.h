efe2a61b440f585ae2b69178d0a8b1f0c5601328
darylldawn <daryllvillaos@gmail.com>
2017-07-04 10:53:44 +0900

    Create CustomUIAlertController.h

---
+++ src/ios/CustomUIAlertController.h
@@ -0,0 +1,16 @@
+#import <UIKit/UIKit.h>
+
+@interface CustomUIAlertController : UIAlertController
+
+@property (nonatomic) UIColor *messageColor;
+@property (nonatomic) UIFont  *messageFont;
+@property (nonatomic) NSTextAlignment messageAlign;
+
+@property (nonatomic) UIColor *titleColor;
+@property (nonatomic) UIFont  *titleFont;
+@property (nonatomic) NSTextAlignment titleAlign;
+
+@property (nonatomic) UIColor *tintColor;
+
+
+@end
