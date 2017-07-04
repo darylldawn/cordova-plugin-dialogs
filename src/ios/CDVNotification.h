2c3e51c0994ef27a49ab087584e99c352188db31
darylldawn <daryllvillaos@gmail.com>
2017-07-04 10:53:23 +0900

    Update CDVNotification.h

--- src/ios/CDVNotification.h
+++ src/ios/CDVNotification.h
@@ -37,17 +37,3 @@
 
 @end
  
-@interface CustomUIAlertController : UIAlertController
-
-@property (nonatomic) UIColor *messageColor;
-@property (nonatomic) UIFont  *messageFont;
-@property (nonatomic) NSTextAlignment messageAlign;
-
-@property (nonatomic) UIColor *titleColor;
-@property (nonatomic) UIFont  *titleFont;
-@property (nonatomic) NSTextAlignment titleAlign;
-
-@property (nonatomic) UIColor *tintColor;
-
-
-@end
