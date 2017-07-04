3027b56aacbd73dd808f4a2607451699f8086bf7
darylldawn <daryllvillaos@gmail.com>
2017-07-04 10:43:01 +0900

    Update CDVNotification.h

--- src/ios/CDVNotification.h
+++ src/ios/CDVNotification.h
@@ -28,10 +28,26 @@
 - (void)confirm:(CDVInvokedUrlCommand*)command;
 - (void)prompt:(CDVInvokedUrlCommand*)command;
 - (void)beep:(CDVInvokedUrlCommand*)command;
+- (void)alertGuide::(CDVInvokedUrlCommand*)command;
 
 @end
 
 @interface CDVAlertView : UIAlertView {};
 @property (nonatomic, copy) NSString* callbackId;
+
+@end
+ 
+@interface CustomUIAlertController : UIAlertController
+
+@property (nonatomic) UIColor *messageColor;
+@property (nonatomic) UIFont  *messageFont;
+@property (nonatomic) NSTextAlignment messageAlign;
+
+@property (nonatomic) UIColor *titleColor;
+@property (nonatomic) UIFont  *titleFont;
+@property (nonatomic) NSTextAlignment titleAlign;
+
+@property (nonatomic) UIColor *tintColor;
+
 
 @end
